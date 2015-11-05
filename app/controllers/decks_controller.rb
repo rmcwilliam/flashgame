d1de5b1a2d6b43da378ece70cb354ca41e5249c7
robertcramer <robertbcramer@icloud.com>
2015-11-05 16:40:31 -0500

    wrapping up the day

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,6 +1,14 @@
 class DecksController < ApplicationController
 
   def create
+    @deck = Deck.new(title: params[:title],
+                     user_id: params[:user_id])
+    if @deck.save
+      render "create.json.jbuilder", status: :created
+    else
+      render json: { errors: @deck.errors.full_messages },
+             status: :unprocessable_entity
+    end
   end
 
   def index
@@ -10,6 +18,13 @@ class DecksController < ApplicationController
   end
 
   def destroy
+    @deck = Deck.find_by(title: params[:title])
+    if current_user.id && @deck.user_id(params[:user_id])
+      @deck.destroy
+    else
+      render json: { error: "Unable to delete the deck." },
+             status: :unauthorized
+    end
   end
 
   def edit
