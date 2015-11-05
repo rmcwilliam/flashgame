1b1f3b017403052417ce80fff07385b4fe316359
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-05 09:51:54 -0500

    added user model

---
+++ db/migrate/20151105143722_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :fullname, null: false
+      t.string :email, null: false
+      t.string :paswword_digest, null: false
+      t.string :access_token
+
+      t.timestamps null: false
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :fullname,  
+  end
+end
