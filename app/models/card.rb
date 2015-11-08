629704b144946a574c2debfb38bbf3a874fab577
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-08 11:32:18 -0500

    added some validations to models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,8 @@
 class Card < ActiveRecord::Base
+  validates :deck_id, presence: true
+  validates :question, presence: true
+  validates :answer, presence: true
+
   belongs_to :deck
   has_many :guesses
   has_many :users, through: :guesses
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,8 @@
 class Guess < ActiveRecord::Base
+  validates :card_id, presence: true
+  validates :user_id, presence: true
+  validates :correct, presence: true
+
   belongs_to :user
   belongs_to :card
 end
