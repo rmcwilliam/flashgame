b63448af72e4cb1240027b915c61f665a3399e83
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-10 11:21:11 -0500

    refactor; rescue_from

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,19 +13,19 @@ class CardsController < ApplicationController
   end
 
   def index
-    @deck = Deck.find_by(id: params[:deck_id])
+    @deck = Deck.find(params[:deck_id])
     @cards = @deck.cards
     render "index.json.jbuilder", status: :accepted
   end
 
 
   def show
-    @card = Card.find_by(id: params[:card_id])
+    @card = Card.find(params[:card_id])
     render "show.json.jbuilder", status: :accepted 
   end
 
   def destroy
-     @card = Card.find_by(id: params[:card_id])
+     @card = Card.find(params[:card_id])
      if @card && current_user.id == @card.deck.user_id
        @card.destroy
         render json: {success: "Card delete successful!"}, status: :accepted 
@@ -35,7 +35,7 @@ class CardsController < ApplicationController
   end
 
   def update
-    @card = Card.find_by(id: params[:card_id])
+    @card = Card.find(params[:card_id])
     if @card && current_user.id == @card.deck.user_id
      @card.update(question: params[:question], answer: params[:answer])
       render json: {success: "Title: #{@card.question} Answer: #{@card.answer}"},
