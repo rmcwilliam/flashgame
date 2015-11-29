6087e2ee2ab569da66d030097cc725d756c60b1c
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-29 18:14:30 -0500

    edit users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,15 @@
 FactoryGirl.define do
-  factory :users do
+  factory :user do
     fullname 'ryan mcwilliam'
     username 'rmcwilliam'
     email 'rmcwilliam@gmail.com'
+    password_digest 'passwordpassword'
 
     trait :julia do
       fullname 'julia kofron'
       username 'jkofron'
       email 'jkofron@gmail.com'
+      password_digest 'passwordpassword'
     end
   end
 end
